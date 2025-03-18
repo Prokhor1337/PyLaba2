18068a15613a8f52146d3499d9805652b831433c
Prokhor1337 <chertilin2006@gmail.com>
2025-03-18 18:30:58 +0000

    Create z1.py

---
+++ z1.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def convert_temperature():
+    try:
+        celsius_value = float(entry.get())
+        fahrenheit_value = celsius_value * 9/5 + 32
+        result_label.config(text=f"Температура в Фаренгейтах: {fahrenheit_value:.2f}")
+    except ValueError:
+        result_label.config(text="Будь ласка, введіть число")
+
+root = tk.Tk()
+root.title("Конвертер температури")
+
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+convert_button = tk.Button(root, text="Конвертувати", command=convert_temperature)
+convert_button.pack(pady=5)
+
+result_label = tk.Label(root, text="")
+result_label.pack(pady=10)
+
+root.mainloop()
